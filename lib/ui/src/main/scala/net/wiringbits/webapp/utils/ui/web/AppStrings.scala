449dd9ec741d124ac2c10b21c9fd09454a11bbda
Alexis Hernandez <alexis22229@gmail.com>
2021-11-20 20:23:42 -0700

    Remove useless files

--- lib/ui/src/main/scala/net/wiringbits/webapp/utils/ui/web/AppStrings.scala
+++ lib/ui/src/main/scala/net/wiringbits/webapp/utils/ui/web/AppStrings.scala
@@ -1,12 +1,6 @@
 package net.wiringbits.webapp.utils.ui.web
 
 object AppStrings {
-  val appName = "Wiringbits"
-  val appMotto = "be the best"
-
-  val home = "Home"
   val tables = "Tables"
-  val dataExplorer = "Data Explorer"
-
   val loading = "Loading"
 }
--- lib/ui/src/main/scala/net/wiringbits/webapp/utils/ui/web/utils/package.scala
+++ lib/ui/src/main/scala/net/wiringbits/webapp/utils/ui/web/utils/package.scala
@@ -3,12 +3,20 @@ package net.wiringbits.webapp.utils.ui.web
 package object utils {
 
   private def upperCaseFirstLetter(word: String): String = {
+    // TODO: This likely does the same but it is safer
+    //     word.headOption
+    //      .map(_.toUpper)
+    //      .map(_ + word.tail)
+    //      .getOrElse("")
     // This replaces every ocurrence
     word.replace(word.head, word.head.toUpper)
   }
 
+  // TODO: This converts "snake_case" to "Upper Case" words, let's get a proper name for it
   def formatField(word: String): String = {
-    val splittedArray = word.split("_")
-    splittedArray.map(upperCaseFirstLetter).mkString(" ")
+    word
+      .split("_")
+      .map(upperCaseFirstLetter)
+      .mkString(" ")
   }
 }
