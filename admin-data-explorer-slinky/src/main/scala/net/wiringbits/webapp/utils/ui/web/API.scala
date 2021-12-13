a4930c6e661505709f0fd0492f0145367c3155bc
caiseL <cristophertraperoo@gmail.com>
2021-12-13 10:43:52 -0700

    Replaced ExecutionContext for MacrotaskExecutor

--- admin-data-explorer-slinky/src/main/scala/net/wiringbits/webapp/utils/ui/web/API.scala
+++ admin-data-explorer-slinky/src/main/scala/net/wiringbits/webapp/utils/ui/web/API.scala
@@ -2,13 +2,13 @@ package net.wiringbits.webapp.utils.ui.web
 
 import net.wiringbits.webapp.utils.api.AdminDataExplorerApiClient
 
-import scala.concurrent.ExecutionContext
+import org.scalajs.macrotaskexecutor.MacrotaskExecutor.Implicits._
 
 case class API(client: AdminDataExplorerApiClient)
 
 object API {
 
-  def apply()(implicit ec: ExecutionContext): API = {
+  def apply(): API = {
     implicit val sttpBackend = sttp.client.FetchBackend()
     val client = new AdminDataExplorerApiClient.DefaultImpl(AdminDataExplorerApiClient.Config(""))
     API(client)
