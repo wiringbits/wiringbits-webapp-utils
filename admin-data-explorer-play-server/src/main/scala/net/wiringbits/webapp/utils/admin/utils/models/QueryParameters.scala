94e7edae8fc1fcfcf713fca0ed5912ddeb04ea56
Sebastian <seb02tian20@gmail.com>
2023-02-01 19:53:40 -0700

    Better QueryParameters toString

--- admin-data-explorer-play-server/src/main/scala/net/wiringbits/webapp/utils/admin/utils/models/QueryParameters.scala
+++ admin-data-explorer-play-server/src/main/scala/net/wiringbits/webapp/utils/admin/utils/models/QueryParameters.scala
@@ -2,6 +2,7 @@ package net.wiringbits.webapp.utils.admin.utils.models
 
 case class QueryParameters(sort: SortParameter, pagination: PaginationParameter, filters: List[FilterParameter]) {
   override def toString: String = {
-    s"sort = ${sort.field} ${sort.ordering}, pagination = ${pagination.start} to ${pagination.end}, filters = $filters"
+    s"sort = ${sort.field} ${sort.ordering}, pagination = ${pagination.start} to ${pagination.end}, filters = ${filters
+        .mkString("{", ", ", "}")}"
   }
 }
